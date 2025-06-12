90c8307d812d9a64b1781b5087ef3e480d8a5df3
LeoSabino <112209471+LeoSabino@users.noreply.github.com>
2025-06-12 16:22:46 -0300

    feature - 20 - export files (#30)
    * Dando rebase com a dev
    
    * Alterando filtros de busca
    
    * Alterando filtros de busca
    
    * fix export
    
    ---------
    
    Co-authored-by: mathias <mathiaskaufff@gmail.com>

--- app/domain/repositories/suspeitorepository.py
+++ app/domain/repositories/suspeitorepository.py
@@ -100,5 +100,16 @@ class ISuspeitoRepository(ABC):
         pass
 
     @abstractmethod
-    def buscar_por_filtro(self, filtro: FiltroDTO) -> List[Suspeito]:
+    def buscar_por_filtro(
+            self,
+            numeros: Optional[List[str]],
+            suspeitos: Optional[List[str]],
+            operacoes: Optional[List[str]],
+            grupo: Optional[str],
+            tipo: Optional[str],
+            data_inicial: Optional[str],
+            data_final: Optional[str],
+            hora_inicio: Optional[str],
+            hora_fim: Optional[str]
+    ) -> List[Suspeito]:
         raise NotImplementedError
